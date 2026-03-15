64e6cba117e55619798763cae7e1257a05376c70
Menaluc <menalu3344@gmail.com>
2026-03-15 11:14:23 +0200

    Initial Express backend setup with predict route

--- src/routes/predict.routes.js
+++ src/routes/predict.routes.js
@@ -1,6 +1,5 @@
 const express = require('express') // import
 const router = express.Router() // creat router
-
 router.post("/predict", (req, res) => {
     res.json({ "message": "Prediction endpoint working" });
 
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const app = require('./app');
 const port = 3000;
 const server = http.createServer(app);
-
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
